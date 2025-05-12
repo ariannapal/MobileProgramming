8df8fa87baa5b1227fd7cefb536fd9e60f1b8b4d
ariannapal <ari19arianna@gmail.com>
2025-05-12 20:33:46 +0200

    header colorato

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,47 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+
 export default function TabsLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="home" options={{ title: "Home" }} />
-      <Tabs.Screen name="categorie" options={{ title: "Categorie" }} />
-      <Tabs.Screen name="statistiche" options={{ title: "Statistiche" }} />
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          headerStyle: {
+            backgroundColor: "#6a11cb", // Colore di sfondo dell'header
+          },
+          headerTintColor: "#fff", // Colore del testo dell'header
+        }}
+      />
+      <Tabs.Screen
+        name="categorie"
+        options={{
+          title: "Categorie",
+          headerStyle: {
+            backgroundColor: "#6a11cb", // Colore di sfondo dell'header
+          },
+          headerTintColor: "#fff", // Colore del testo dell'header
+        }}
+      />
+      <Tabs.Screen
+        name="statistiche"
+        options={{
+          title: "Statistiche",
+          headerStyle: {
+            backgroundColor: "#6a11cb", // Colore di sfondo dell'header
+          },
+          headerTintColor: "#fff", // Colore del testo dell'header
+        }}
+      />
       <Tabs.Screen
         name="preferiti"
         options={{
           title: "Preferiti",
+          headerStyle: {
+            backgroundColor: "#6a11cb", // Colore di sfondo dell'header
+          },
+          headerTintColor: "#fff", // Colore del testo dell'header
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="heart" size={size} color={color} />
           ),
